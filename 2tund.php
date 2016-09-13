83d3e27e77fd84e8dd74b1642c258072074c3a9e
juriguss <juriguss@greeny.cs.tlu.ee>
2016-09-13 13:10:32 +0300

    if statement

---
+++ 2tund.php
@@ -0,0 +1,35 @@
+<?php
+
+     $age = 10;
+	 
+	 
+	 if ($age < 18) {
+		 echo "alaealine";
+		 
+		 
+	
+	 } else {
+	
+	echo "taisealine";
+	
+	}
+	
+
+
+
+?>
+
+<br>
+
+<?php
+//vanusele vastavalt trukime valja palju
+for ($i = 0; $i < $age; $i = $i + 1) {
+	
+	echo $i."palju ";
+	
+}
+
+echo "onne";
+
+
+?>
